dfd0c9e15e13c002eb993f705403b791d2e270f1
kano1201 <kano.nakamura3@gmail.com>
2021-08-04 08:15:52 +0000

    add_gem.controller.model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :partner_name, :anniversary, :gender, :partner_gender, :couple_name, :profile_image_id, :is_deleted])
+  end
 end
