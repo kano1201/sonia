091d905b94a0e041bf5ab50532dfe8bc30d59231
kano1201 <kano.nakamura3@gmail.com>
2021-08-09 06:17:20 +0000

    add_relationship.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,21 @@ class User < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_many :reverse_of_relationshios, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #自分がフォローされる側
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #自分がフォローする側
+  has_many :followers, through: :reverse_of_relationshios, source: :follower #自分をフォローしてるユーザー
+  has_many :followings, through: :relationships, source: :followed #自分がフォローしてるユーザー
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'about' => 'homes#about'
-  resources :users, only: [:show, :edit, :update, :index, :unsubscribe, :withdraw]
+
+  resources :users, only: [:show, :edit, :update, :index, :unsubscribe, :withdraw] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as 'followers'
+  end
+
   resources :photos, only: [:show, :edit, :update, :index, :destroy, :new, :create] do
     resource :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
