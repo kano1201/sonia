dd4ac7bf6e7b1b30a118ff8bc36906321a106f8e
kano1201 <kano.nakamura3@gmail.com>
2021-08-12 05:45:23 +0000

    ajax_likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,11 @@ class LikesController < ApplicationController
     @photo = Photo.find(params[:photo_id])
     like = current_user.likes.new(photo_id: @photo.id)
     like.save
-    redirect_to request.referer
   end
 
   def destroy
     @photo = Photo.find(params[:photo_id])
     like = current_user.likes.find_by(photo_id: @photo.id)
     like.destroy
-    redirect_to request.referer
   end
 end
