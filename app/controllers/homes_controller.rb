05183f59f7090e6a07ac906e8831c1c57e4815e1
kano1201 <kano.nakamura3@gmail.com>
2021-08-14 12:11:07 +0000

    add_kaminari.top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def top
-    @photos = Photo.all
-    @users = User.all
+    @photos = Photo.page(params[:page]).reverse_order
+    @users = User.page(params[:page]).reverse_order
   end
 
   def about
