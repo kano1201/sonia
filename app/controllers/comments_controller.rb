0d96617bdd9541661dbf96530a37410e820ebd8e
kano1201 <kano.nakamura3@gmail.com>
2021-08-14 08:39:33 +0000

    fix_ajax.comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,23 +6,18 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.photo_id = @photo.id
     @comment.user_id = current_user.id
-    if @comment.save
-      redirect_to photo_path(@photo.id)
-    else
-      render 'photos/show'
-    end
+    @comment.save
   end
 
   def destroy
     @photo = Photo.find(params[:photo_id])
     comment = @photo.comments.find(params[:id])
     comment.destroy
-    redirect_to request.referer
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :photo_id, :user_id)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :photo
+
+  validates :comment, presence: true
+
 end
