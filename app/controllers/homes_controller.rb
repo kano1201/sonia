c86a89241abb24686cb47aca58aacc126d6042cc
kano1201 <kano.nakamura3@gmail.com>
2021-08-24 09:19:13 +0000

    退会user消す

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def top
     @photos = Photo.limit(5).order(" created_at DESC ")
-    @users = User.limit(5).order(" created_at DESC ")
+    @users = User.limit(5)
   end
 
   def about
